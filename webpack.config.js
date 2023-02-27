bf1d35e71b6caf14e7ea4f363583dd5312de3916
Verdadewoo <107331033+Verdadewoo@users.noreply.github.com>
2023-02-27 14:36:58 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/vuetifymessenger_001/" : "/",
     resolve: {
         alias: {
             '@': path.resolve('resources/js'),
